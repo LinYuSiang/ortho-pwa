dc168d80c8b28b642aa31a3d51a0646b75ffb52a
Wayne Huang <s1410432005@gms.nutc.edu.tw>
2019-03-13 17:23:25 +0800

    add navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <v-app>
     <v-toolbar>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>{{ $route.name }}</v-toolbar-title>
     </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click="$router.push({ path: item.path })"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -12,11 +33,17 @@
 
 <script>
 export default {
-  name: 'App',
-  data () {
-    return {
-      //
-    }
-  }
-}
+  name: "App",
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "首頁", icon: "dashboard", path: "/" },
+      {
+        title: "人工膝關節置換術中測量",
+        icon: "assignment",
+        path: "/knee-joint"
+      }
+    ]
+  })
+};
 </script>
