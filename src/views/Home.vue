184a04c77a004827bd567508d2c44984facd468a
LinYuSiang <zz020411569@gmail.com>
2019-05-21 17:01:15 +0800

    modify home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <v-container grid-list-sm fluid>
     <v-layout wrap>
-      <v-flex sm4 >
+      <v-flex sm3 offset-sm1 >
         <v-btn  color="info" @click="$router.push({ name:'創建knee' })"  large >創建 knee joint </v-btn>
       </v-flex>
       <!-- <v-flex sm></v-flex> -->
-      <v-flex sm4>
+      <v-flex sm3 offset-sm1>
         <v-btn style="width:173.19px" color="info" @click="$router.push({ name:'創建hip' })"  large >創建 hip joint </v-btn>
       </v-flex>
       <!-- <v-flex sm1></v-flex> -->
-       <v-flex sm4>
+       <v-flex sm3 offset-sm1>
         <v-btn style="width:173.19px" color="info" @click="$router.push({ name:'查詢' })"  large >查詢 </v-btn>
       </v-flex>
       <!-- <v-flex sm1></v-flex> -->
